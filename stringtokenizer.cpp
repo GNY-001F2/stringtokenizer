24728eb660c28323819ac9c0d2eb83111363c31a
Aayush Agarwal <aayush.agarwal_wu@yahoo.com>
2016-05-01 22:13:35 -0400

    moved template function impementation into header as inline function

--- stringtokenizer.cpp
+++ stringtokenizer.cpp
@@ -19,53 +19,6 @@
 
 namespace stringtokenizer {
 
-/* WARNING: It is your responsibility to ensure that the integers in the
- * strings sent to these functions do not exceed the size of the type of
- * integer vector the function returns as that could lead to undefined
- * behaviour.
- * To ensure transparent string conversion stoll and stoull are used 
- * and then demoted to your preferred type.
- */
-
-/* WARNING: DO NOT ask string_tokesn_to_int() to convert to unsigned values;
- * that is unexpected behaviour and generally not recommended, even if it
- * works with smaller integers. Just use string_tokens_to_uint() instead and
- * save yourself from any potential problems.
- */
-template <typename intT> std::vector<intT>
-string_tokens_to_int(std::string source, char delimiter) {
-
-    std::vector<std::string> tokens = tokenize(source, delimiter);
-    return string_tokens_to_int<intT>(tokens);
-}
-
-template <typename intT> std::vector<intT> 
-string_tokens_to_int(std::vector<std::string> tokens) {
-
-    std::vector<intT> int_tokens(0);
-    for(const auto& i: tokens)
-        int_tokens.push_back(static_cast<intT>(std::stoll(i)));
-    return int_tokens;
-}
-
-template <typename UintT> std::vector<UintT>
-string_tokens_to_uint(std::string source, char delimiter) {
-
-    std::vector<std::string> tokens = tokenize(source, delimiter);
-    return string_tokens_to_uint<UintT>(tokens);
-}
-
-template <typename UintT> std::vector<UintT>
-string_tokens_to_uint(std::vector<std::string> tokens) {
-
-    std::vector<UintT> uint_tokens(0);
-    for(const auto& i: tokens)
-        uint_tokens.push_back(static_cast<UintT>(std::stoull(i)));
-    return uint_tokens;
-}
-
-/*TODO: implement functions for floating point values */
-
 /* WARNING: tokenize is currently only tested to work with standard strings
  * where each character fits in a byte. Using unicode or any other multi-byte
  * encodings may result in unexpected behaviour.
--- stringtokenizer.h
+++ stringtokenizer.h
@@ -29,7 +29,7 @@ namespace stringtokenizer {
  * converted from a source string as segregated by the delimiter of your
  * choice with the default being spaces
  */
-template <typename intT> std::vector<intT>
+template <typename intT> inline std::vector<intT>
 string_tokens_to_int(std::string source, char delimiter = ' ');
 
 /* string_tokens_to_int(std::vector<std::string> tokens):
@@ -41,7 +41,7 @@ string_tokens_to_int(std::string source, char delimiter = ' ');
  * skip = true to skip type size checking and process each number as if it were
  * a long long value and then cast it to
  */
-template <typename intT> std::vector<intT> 
+template <typename intT> inline std::vector<intT>
 string_tokens_to_int(std::vector<std::string> tokens);
 
 /* string_tokens_to_uint(std::string source, char delimiter):
@@ -49,14 +49,14 @@ string_tokens_to_int(std::vector<std::string> tokens);
  * type converted from a source string as segregated by the delimiter of your
  * choice with the default being a space
  */
-template <typename UintT> std::vector<UintT>
+template <typename UintT> inline std::vector<UintT>
 string_tokens_to_uint(std::string source, char delimiter = ' ');
 
 /* string_tokens_to_int(std::vector<std::string> tokens):
  * template function that returns a vector of your specified unsigned
  * integer type converted from a source string vector
  */
-template <typename UintT> std::vector<UintT>
+template <typename UintT> inline std::vector<UintT>
 string_tokens_to_uint(std::vector<std::string> tokens);
 
 /*TODO: define functions for float values */
@@ -65,6 +65,53 @@ string_tokens_to_uint(std::vector<std::string> tokens);
  * the delimiter, with the default being a space.
  */
 std::vector<std::string> tokenize(std::string source, char delimiter = ' ');
+
+/* WARNING: It is your responsibility to ensure that the integers in the
+ * strings sent to these functions do not exceed the size of the type of
+ * integer vector the function returns as that could lead to undefined
+ * behaviour.
+ * To ensure transparent string conversion stoll and stoull are used 
+ * and then demoted to your preferred type.
+ */
+
+/* WARNING: DO NOT ask string_tokesn_to_int() to convert to unsigned values;
+ * that is unexpected behaviour and generally not recommended, even if it
+ * works with smaller integers. Just use string_tokens_to_uint() instead and
+ * save yourself from any potential problems.
+ */
+template <typename intT> inline std::vector<intT>
+string_tokens_to_int(std::string source, char delimiter) {
+
+    std::vector<std::string> tokens = tokenize(source, delimiter);
+    return string_tokens_to_int<intT>(tokens);
+}
+
+template <typename intT> inline std::vector<intT> 
+string_tokens_to_int(std::vector<std::string> tokens) {
+
+    std::vector<intT> int_tokens(0);
+    for(const auto& i: tokens)
+        int_tokens.push_back(static_cast<intT>(std::stoll(i)));
+    return int_tokens;
+}
+
+template <typename UintT> inline std::vector<UintT>
+string_tokens_to_uint(std::string source, char delimiter) {
+
+    std::vector<std::string> tokens = tokenize(source, delimiter);
+    return string_tokens_to_uint<UintT>(tokens);
+}
+
+template <typename UintT> inline std::vector<UintT>
+string_tokens_to_uint(std::vector<std::string> tokens) {
+
+    std::vector<UintT> uint_tokens(0);
+    for(const auto& i: tokens)
+        uint_tokens.push_back(static_cast<UintT>(std::stoull(i)));
+    return uint_tokens;
+}
+
+/*TODO: implement functions for floating point values */
 
 }
 
