d91951dd8c2c58f380352586c22f894a850fba1b
Aayush Agarwal <aayush.agarwal_wu@yahoo.com>
2016-05-02 11:46:32 -0400

    completely rewrote vector functions to mirror the std::sto<type> family and added some basic exception handling for the exceptions thrown by std::sto<type> family of functions

--- stringtokenizer.cpp
+++ stringtokenizer.cpp
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdexcept>
 #include "stringtokenizer.h"
 
 namespace stringtokenizer {
@@ -42,8 +43,139 @@ std::vector<std::string> tokenize(std::string source, char delimiter) {
     return stringtokens;
 }
 
-// template <long long> std::vector<long long>
-// string_tokens_to_int(std::string source, char delimiter);
-// template <long long> std::vector<long long>
-// string_tokens_to_int(std::vector<std::string> tokens);
+/* WARNING: It is your responsibility to ensure that the values in the
+ * strings sent to these functions are not out of range.
+ *
+ * These functions will not add any values that are out of range to the vector.
+ * These functions can handle mixed input using the same rules as the
+ * std::sto<type> functions.
+ */
+
+/* functions that take string and delimiter as input */
+std::vector<int> sttoiv(std::string source, char delimiter) {
+
+    std::vector<std::string> tokens = tokenize(source, delimiter);
+    return sttoiv(tokens);
+}
+std::vector<long> sttolv(std::string source, char delimiter) {
+
+    std::vector<std::string> tokens = tokenize(source, delimiter);
+    return sttolv(tokens);
+}
+std::vector<long long> sttollv(std::string source, char delimiter) {
+
+    std::vector<std::string> tokens = tokenize(source, delimiter);
+    return sttollv(tokens);
+}
+
+std::vector<unsigned long> sttoulv(std::string source, char delimiter) {
+
+    std::vector<std::string> tokens = tokenize(source, delimiter);
+    return sttoulv(tokens);
+}
+std::vector<unsigned long long> sttoullv(std::string source, char delimiter) {
+
+    std::vector<std::string> tokens = tokenize(source, delimiter);
+    return sttoullv(tokens);
+}
+
+std::vector<float> sttofv(std::string source, char delimiter) {
+
+    std::vector<std::string> tokens = tokenize(source, delimiter);
+    return sttofv(tokens);
+}
+std::vector<double> sttodv(std::string source, char delimiter) {
+
+    std::vector<std::string> tokens = tokenize(source, delimiter);
+    return sttodv(tokens);
+}
+std::vector<long double> sttoldv(std::string source, char delimiter) {
+
+    std::vector<std::string> tokens = tokenize(source, delimiter);
+    return sttoldv(tokens);
+}
+
+/* functions that take string vectors as input */
+std::vector<int> sttoiv(std::vector<std::string> tokens) {
+
+    std::vector<int> int_tokens(0);
+    for(const auto& i: tokens) {
+        try { int_tokens.push_back(std::stoi(i)); }
+        catch(std::invalid_argument) { continue; }
+        catch(std::out_of_range) { continue; }
+    }
+    return int_tokens;
+}
+std::vector<long> sttolv(std::vector<std::string> tokens) {
+
+    std::vector<long> long_tokens(0);
+    for(const auto& i: tokens) {
+        try { long_tokens.push_back(std::stol(i)); }
+        catch(std::invalid_argument) { continue; }
+        catch(std::out_of_range) { continue; }
+    }
+    return long_tokens;
+}
+std::vector<long long> sttollv(std::vector<std::string> tokens) {
+
+    std::vector<long long> long_long_tokens(0);
+    for(const auto& i: tokens) {
+        try { long_long_tokens.push_back(std::stoll(i)); }
+        catch(std::invalid_argument) { continue; }
+        catch(std::out_of_range) { continue; }
+    }
+    return long_long_tokens;
+}
+
+std::vector<unsigned long> sttoulv(std::vector<std::string> tokens) {
+
+    std::vector<unsigned long> unsigned_long_tokens(0);
+    for(const auto& i: tokens) {
+        try { unsigned_long_tokens.push_back(std::stoul(i)); }
+        catch(std::invalid_argument) { continue; }
+        catch(std::out_of_range) { continue; }
+    }
+    return unsigned_long_tokens;
+}
+std::vector<unsigned long long> sttoullv(std::vector<std::string> tokens) {
+
+    std::vector<unsigned long long> unsigned_long_long_tokens(0);
+    for(const auto& i: tokens) {
+        try { unsigned_long_long_tokens.push_back(std::stoull(i)); }
+        catch(std::invalid_argument) { continue; }
+        catch(std::out_of_range) { continue; }
+    }
+    return unsigned_long_long_tokens;
+}
+
+std::vector<float> sttofv(std::vector<std::string> tokens) {
+
+    std::vector<float> float_tokens(0);
+    for(const auto& i: tokens) {
+        try { float_tokens.push_back(std::stof(i)); }
+        catch(std::invalid_argument& iarg) { continue; }
+        catch(std::out_of_range& oor) { continue; }
+    }
+    return float_tokens;
+}
+std::vector<double> sttodv(std::vector<std::string> tokens) {
+
+    std::vector<double> double_tokens(0);
+    for(const auto& i: tokens) {
+        try { double_tokens.push_back(std::stod(i)); }
+        catch(std::invalid_argument) { continue; }
+        catch(std::out_of_range) { continue; }
+    }
+    return double_tokens;
+}
+std::vector<long double> sttoldv(std::vector<std::string> tokens) {
+
+    std::vector<long double> long_double_tokens(0);
+    for(const auto& i: tokens) {
+        try { long_double_tokens.push_back(std::stold(i)); }
+        catch(std::invalid_argument) { continue; }
+        catch(std::out_of_range) { continue; }
+    }
+    return long_double_tokens;
+}
 }
